565b72fc80d1b7f6d7f6cc691841d6e141a79d75
Super-ma-nong <331150871@qq.com>
2021-03-25 23:26:55 +0800

    Cplusplus

---
+++ Visual Studio 2019 C++/2021.3.25学习C++/2021.3.25学习C++/同步式练习.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+
+// 用C++验证一个知识点
+// 静态变量只有首次初始化才有用
+//using namespace std;
+//void Add()
+//{
+//	static int n = 0;
+//	n++;
+//	cout << n << endl;
+//	return;
+//}
+//int main()
+//{
+//	Add();// 1
+//	Add();// 2
+//	return 0;
+//}
+//// 至于这是为什么暂时无法解释
+//void Add(static int n)
+//{
+//	n++;
+//	cout << n << endl;
+//	return;
+//}
+//int main()
+//{
+//	Add(1);// 2
+//	Add(1);// 2
+//	return 0;
+//}
+
